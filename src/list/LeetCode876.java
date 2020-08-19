42a15c27f777cdb04e549736a46d605f9b1720e1
coder_elijah <coderelijah@126.com>
2020-08-19 16:33:58 +0800

    LeetCode876 链表中点

--- src/list/LeetCode876.java
+++ src/list/LeetCode876.java
@@ -11,10 +11,9 @@ package list;
  */
 public class LeetCode876 {
     public ListNode middleNode(ListNode head) {
-        ListNode fast = head, slow = head, pre = null;
+        ListNode fast = head, slow = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            pre = slow;
             slow = slow.next;
         }
 
