5c970449bab756a9008356758c0838e2705715af
coder_elijah <coderelijah@126.com>
2020-08-19 21:33:29 +0800

    冒泡排序

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package sort;
+
+/**
+ * Description:
+ *
+ * @author elijahliu
+ * @Note Talk is cheap,just show me ur code.- -!
+ * ProjectName:EAlgorithm
+ * PackageName: sort
+ * Date: 2020/8/19 21:28
+ */
+public class BubbleSort {
+
+    public void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+}
